27da6af43f496524ca28dbc52d78005b86e87b67
akshay <akshay.patel@moreyeahs.co>
2022-12-02 12:44:43 +0530

    vercel

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const helmet=require('helmet');
-const cors = require('cors');
-const compression=require('compression');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+const middlewares = require("./middlewares");
+const api = require("./api");
 
 const app = express();
-
 
 app.use(helmet());
 app.use(cors());
@@ -17,15 +16,15 @@ app.use(express.json());
 
 app.use(compression());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'Welcome to Musico API',
-    "API Docs" :'https://documenter.getpostman.com/view/17456794/U16jPS13#intro',
-    
+    message: "Welcome to Musico API",
+    "API Docs":
+      "https://documenter.getpostman.com/view/17456794/U16jPS13#intro",
   });
 });
 
-app.use('/api/v1', api);
+app.use("/api/v1", api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
