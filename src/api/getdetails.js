48ee4e8b492da9cca713816ec3b411b53067afba
akshay <akshay.patel@moreyeahs.co>
2022-12-07 18:03:54 +0530

    vrcel

--- src/api/getdetails.js
+++ src/api/getdetails.js
@@ -1,38 +1,34 @@
-const express = require('express');
-const { GetDetails } = require('../endpoints');
-const { get } = require('../get');
+const express = require("express");
+const { GetDetails } = require("../endpoints");
+const { get } = require("../get");
 const router = express.Router();
-const cache=require('memory-cache');
+const cache = require("memory-cache");
 
-const caching=new cache.Cache();
-const cacheTime=20*60*1000;//time in mili seconds
+const caching = new cache.Cache();
+const cacheTime = 20 * 60 * 1000; //time in mili seconds
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  const type = req.query.type;
+  const id = req.query.id;
+  const page = req.query.page || 1;
+  const n = isNaN(req.query.n) ? 10 : req.query.n;
+  console.log(type, id, page, n);
+  if (!type || !id) {
+    res.json({ error: "Invalid Arguments" });
+    return;
+  }
 
-      const type=req.query.type;
-      const id=req.query.id;
-      const page=req.query.page || 1;
-      const n=isNaN(req.query.n)?10:req.query.n
-     
-      if(!type || !id){
-          res.json({"error":"Invalid Arguments"});
-          return;
-      }
+  // if (caching.get(id)) {
+  //   res.status(200).json({ data: caching.get(id), source: "cache" });
+  //   return;
+  // }
 
-      if(caching.get(id)){
-        res.status(200).json({"data":caching.get(id),"source":"cache"});
-        return;
-      }
+  let link = GetDetails(id, type, page, n);
+  console.log(link);
+  const response = await get(link);
 
-      let link=GetDetails(id,type,page,n);
-      console.log(link);
-      const response=await get(link);
-
-      caching.put(id,response.data,cacheTime);
-      res.status(200).json({"data":response.data,"source":"API"});
-    
-  
-
-  });
+  caching.put(id, response.data, cacheTime);
+  res.status(200).json({ data: response.data, source: "API" });
+});
 
 module.exports = router;
