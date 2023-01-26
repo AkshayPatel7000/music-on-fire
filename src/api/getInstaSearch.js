f50d81368b60cf02524600e0e8fd0e687f6c504a
akshay <akshay.patel@moreyeahs.co>
2023-01-26 19:23:01 +0530

    gead

--- src/api/getInstaSearch.js
+++ src/api/getInstaSearch.js
@@ -5,12 +5,21 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   const query = req.query.query;
-  console.log("req->>>", req);
+  console.log("req->>>", req.headers["x-ig-app-id"]);
   if (!query) {
     res.json({ error: "Invalid Arguments" });
     return;
   }
-  const response = await get(getInstaSearch(query));
+  let options = {
+    headers: {
+      "x-ig-app-id": "936619743392459",
+    },
+  };
+  if (req.headers["x-ig-app-id"]) {
+    options.headers["x-ig-app-id"] = req.headers["x-ig-app-id"];
+  }
+
+  const response = await get(getInstaSearch(query), options);
   res.status(200).json({ data: response.data, source: "API" });
 });
 
